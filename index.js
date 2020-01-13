9ae17b29daa48118560121fe45931f8cc6ba68b5
Michelle Ranly <meranly8@gmail.com>
2020-01-13 18:30:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ function endGame() {
 function moveDodger(e) {
   // implement me!
   const code = e.which;
-  if([LEFT_ARROW, RIGHT_ARROW].indexOf(key) > -1) {
+  if([LEFT_ARROW, RIGHT_ARROW].indexOf(code) > -1) {
     e.preventDefault();
     e.stopPropagation();
   }
